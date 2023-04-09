bc8c39d2e8cbe5de97acd199a223c7e2521fa37e
alinakhalid01 <alina.khalid01@gmail.com>
2023-04-09 09:39:41 +0500

    assignment chapter 1 & 2

---
+++ assignment-1/assets/js/app.js
@@ -0,0 +1,19 @@
+// Question1
+
+alert(`Welcome!`);
+
+
+// Question2
+
+alert(`Error! Please enter a valid password`);
+
+
+// Question3
+
+alert(`Welcome to JS Land... \nHappy Coding!`);
+
+
+// Question4
+
+alert(`Welcome to JS Land...`);
+alert(`Happy Coding!`);
---
+++ assignment-2/assets/js/app.js
@@ -0,0 +1,61 @@
+// Question1
+
+let userName
+
+
+// Question2
+
+let myName = `Aleena Khalid`;
+
+
+// Question3
+
+let message = ` Hello World`;
+alert(message);
+
+
+// Question4
+
+let stdName = `Jhone Doe`;
+let stdAge = `15 years old`;
+let stdCourse = `Certified Mobile Application Development`;
+
+alert(stdName);
+alert(stdAge);
+alert(stdCourse);
+
+
+
+// Question5
+
+let item = `PIZZA`;
+
+alert(item + `\n` + item[0] + item[1] + item[2] + item[3] + `\n` + item[0] + item[1] + item[2] + `\n` + item[0] + item[1] + `\n` + item[0]);
+
+
+
+// Question 6
+
+let email = `alina.khalid01@gmail.com`;
+
+alert(`My email address is ` + email);
+
+
+// Question 7
+
+let book = `A smarter way to learn JavaScript`;
+
+alert(`I am trying to learn from the book ${book}`);
+
+
+// Question 8
+
+document.write(`Yah! I can write HTML content through JavaScript`);
+
+
+
+// Question 9
+
+let pattern = `▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬`;
+
+alert(`▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬`);
